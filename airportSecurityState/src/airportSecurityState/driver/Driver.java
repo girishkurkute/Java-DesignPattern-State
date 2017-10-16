5ca7257b2db1b2f95e6b0154bad8c25e913283e7
Girish <gkurkut1@binghamton.edu>
2017-10-15 23:49:06 -0400

    Code to read input file done

--- airportSecurityState/src/airportSecurityState/driver/Driver.java
+++ airportSecurityState/src/airportSecurityState/driver/Driver.java
@@ -1,10 +1,41 @@
 package airportSecurityState.driver;
 
+import airportSecurityState.util.FileProcessor;
+
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
+		String inputfile;
+		String currLine;
+		
+		if(args.length !=1)
+		{			
+			throw new RuntimeException("Please provide 1 arguments");
+		}
+		else
+		{
+			////Assigning text file names
+			inputfile = args[0];
+
+		}
+		
+		FileProcessor fprObj = new FileProcessor(inputfile);
+		
+		while((currLine = fprObj.readLine())!= null)
+		{
+			String day, item;
+			String [] arrayInfo = currLine.split(";");
+			day = arrayInfo[0];
+			item = arrayInfo[3];
+			
+			System.out.println(day);
+			System.out.println(item);
+			
+			
+		}
+		
 	}
 
 }
