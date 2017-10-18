6975f8cc38e626a06184b1a5635537c097ef7f81
Girish <gkurkut1@binghamton.edu>
2017-10-18 15:51:23 -0400

    code to write operation ID into output file

--- airportSecurityState/src/airportSecurityState/driver/Driver.java
+++ airportSecurityState/src/airportSecurityState/driver/Driver.java
@@ -4,29 +4,32 @@ import java.util.ArrayList;
 
 import airportSecurityState.airportStates.AirportSecurity;
 import airportSecurityState.util.FileProcessor;
+import airportSecurityState.util.Results;
 
 public class Driver {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
-		String inputfile;
+		String inputfile,outputfile;
 		String currLine;
 		ArrayList<Integer> TotalDaysList = new ArrayList<Integer>();
 		
-		if(args.length !=1)
+		if(args.length !=2)
 		{			
-			throw new RuntimeException("Please provide 1 arguments");
+			throw new RuntimeException("Please provide 2 arguments");
 		}
 		else
 		{
 			////Assigning text file names
 			inputfile = args[0];
+			outputfile = args[1];
 
 		}
 		
 		FileProcessor fprObj = new FileProcessor(inputfile);
 		AirportSecurity aptObj = new AirportSecurity();
+		Results resltObj = new Results(outputfile);
 		
 		while((currLine = fprObj.readLine())!= null)
 		{
@@ -35,7 +38,7 @@ public class Driver {
 			
 			
 		}
-		
+		aptObj.printID(resltObj);
 	}
 
 }
--- airportSecurityState/src/airportSecurityState/util/MyLogger.java
+++ airportSecurityState/src/airportSecurityState/util/MyLogger.java
@@ -2,4 +2,5 @@ package airportSecurityState.util;
 
 public class MyLogger {
 
+	
 }
